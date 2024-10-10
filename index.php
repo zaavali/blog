68bd44ac6e8e249351ae9653050169a914aa6b59
zaavali <buveusechallbroo95@gmail.com>
2024-10-10 09:46:36 +0200

    show articles

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="bg-secondary p-4">
+        <h1 class="text-center">BLOG</h1>
+        <div class="text-end">
+            <a href="" class="text-light">Administration</a>
+        </div>
+
+
+    </header>
+    <main class="container">
+        <h2 class="text-succes">
+            PHP
+        </h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, repellat deserunt officia eos distinctio, omnis sequi adipisci hic quasi unde tempore saepe velit corrupti, assumenda iusto excepturi tenetur. Sequi, voluptatibus.</p>
+        <div>
+            Créé par CLTR LE GOAT
+        </div>
+        <hr>
+
+        <h2 class="text-succes">
+            JS
+        </h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, repellat deserunt officia eos distinctio, omnis sequi adipisci hic quasi unde tempore saepe velit corrupti, assumenda iusto excepturi tenetur. Sequi, voluptatibus.</p>
+        <div>
+            Créé par LEO
+        </div>
+        <main>
+</body>
+</html>
